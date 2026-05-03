83c6521caeb4059521b6e2ca2a1f4d94eb84510b
Slarti @ DURY LEGAL <slarti@dury.de>
2026-05-03 22:38:14 +0200

    Initial commit: index.js

---
+++ index.js
@@ -0,0 +1,3 @@
+// euipo-fees - EUIPO fees calculator - Fee estimation for EU trademark registration. Powered by DURY LEGAL.
+// Coming soon. Visit https://www.dury.de for more information.
+module.exports = {};
